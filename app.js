0b2b564d5f1e5f823e3950c8cc7f6d5f2abd069a
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-01-27 22:23:23 +0000

    app.js created to fetch the data from my laravel api and disaply them/erorrs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", async() => {
    const apiUrl = "http://localhost:8000/api/birth-certificates";
    const birthCertificatesDiv = document.querySelector("#birth_certificates ul");
 
+   try {
+
    //fetching the data which will return a promise
    const data = await fetch(apiUrl);
 
@@ -11,6 +13,17 @@ document.addEventListener("DOMContentLoaded", async() => {
    }
 
    const birthCertificate = await data.json();
-   
+   birthCertificate.foreach(certificate => {
+    const listItem = document.createElement("li");    //create the li to hold each response in a list
+    listItem.textContent = 'Name: ${birth_certificates.first_name}, Date of Birth ${birth_certificates.birth_date}, Health Status ${birth_certificates.health_status}';
+    birthCertificatesDiv.appendChild(listItem);   //add the child element and append it to the div 
+});
+   } catch(error) {
+    console.log("Error fetching data");
+    const errorMessage = document.createElement("p");
+    errorMessage.textContent = "Unbale to list the birth certificates from the API";
+    birthCertificatesDiv.appendChild(errorMessage);
+   }
+
 
 });
