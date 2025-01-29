e2c9995e3278bf190922d54447cd014bd3a580d0
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-01-29 23:34:47 +0000

    changes made to checking the daata is an array and fixed formatting off outputted data

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
-document.addEventListener("DOMContentLoaded", async() => {
-   const apiUrl = "http://localhost:8000/api/birth_certificates";
-   const birthCertificatesDiv = document.querySelector("#birth_certificates ul");
+document.addEventListener("DOMContentLoaded", async () => {
+   
+    const apiurl = "http://localhost:8000/api/birth_certificates";
+    const birthCertificateDiv = document.querySelector("#birth_certificates ul");
 
-   try {
+    try {
+        const response = await fetch(apiurl);
 
-   //fetching the data which will return a promise
-   const data = await fetch(apiUrl);
+        if(!response.ok) {
+            throw new Error ("Error feteching data");
+        }
 
-   //verify that the data was fetched if not throw error
-   if(!data.ok) {
-    throw new Error("Error fetching the API data");
-   }
+        const birthCertificates = await response.json();
+          
+        //logging the fetched data
+        console.log("Fetched data: ", birthCertificates);
+          
+        //check if the fetched data is an array and the is data in it, if not throw error again 
+        
+        if(Array.isArray(birthCertificates) && birthCertificates.length > 0) {
+            //go through each certifiicate and index it
+            birthCertificates.forEach(certificate => {
+             const certList = document.createElement("li");
+             certList.textContent = `Name: ${certificate.first_name}, Health Status: ${certificate.health_status}, Hospital code: ${certificate.hospital_code}, Date of birth: ${certificate.birth_date}`;
+             birthCertificateDiv.appendChild(certList);
+            });
+        } else {
+            //was not an array or was empty
+            const errorMessage = document.createElement("p");
+            errorMessage.textContent = "Was not an array or was empty";
+            birthCertificateDiv.appendChild(errorMessage);
+        }
 
-   const birthCertificate = await data.json();
-   birthCertificate.forEach(certificate => {
-    const listItem = document.createElement("li");    //create the li to hold each response in a list
-    listItem.textContent = 'Name: ${birth_certificates.first_name}, Date of Birth ${birth_certificates.birth_date}, Health Status ${birth_certificates.health_status}';
-    birthCertificatesDiv.appendChild(listItem);   //add the child element and append it to the div 
-});
-  
-} catch(error) {
-    console.log("Error fetching data");
-    const errorMessage = document.createElement("p");
-    errorMessage.textContent = "Unable to list the birth certificates from the API";
-    birthCertificatesDiv.appendChild(errorMessage);
-   }
+          } catch(error) {
+            console.log("error fetching data");
+            const errorMessage = document.createElement("p");
+            errorMessage.textContent = "Unable to list the birth certificates.";
+            birthCertificateDiv.appendChild(errorMessage);
+          } 
 
 
 });
